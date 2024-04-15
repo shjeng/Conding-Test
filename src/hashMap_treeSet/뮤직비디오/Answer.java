f8f933e60f4295adba1629a61c4119b763826521
shjeng <ghsmsl20@gmail.com>
2024-04-15 18:48:19 +0900

    뮤직비디오(결정알고리즘) 답

---
+++ src/hashMap_treeSet/뮤직비디오/Answer.java
@@ -0,0 +1,49 @@
+package hashMap_treeSet.뮤직비디오;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Answer {
+    // lt부터 rt 사이에 무조건 답이 있어야만 사용할 수 있는 풀이.
+    // 최소값은 9, 최대값은 1~9 모두 합한 값인 45
+    // mid를 찾아야함 (27)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println();
+    }
+
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    public static int count(int[] arr, int capacity) {
+        int cnt = 1, sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else {
+                sum += x;
+            }
+        }
+        return cnt;
+    }
+}
