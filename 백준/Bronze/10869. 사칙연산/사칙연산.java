ce354702962f0854d0afb21a743fc4cb5f0255fa
Jeong Sihwa <134994387+shjeng@users.noreply.github.com>
2024-09-02 21:22:46 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer sto = new StringTokenizer(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        int A = Integer.parseInt(sto.nextToken());
+        int B = Integer.parseInt(sto.nextToken());
+        stringBuilder.append(A + B).append("\n");
+        stringBuilder.append(A - B).append("\n");
+        stringBuilder.append(A * B).append("\n");
+        stringBuilder.append(A / B).append("\n");
+        stringBuilder.append(A % B).append("\n");
+
+        System.out.println(stringBuilder);
+    }
+}
