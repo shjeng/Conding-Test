cbf7761f9383842bd90c1cc1904b35a849285b64
shjeng <ghsmsl20@gmail.com>
2025-03-30 17:29:53 +0900

    허브디앤씨 순열의개수

---
+++ src/hubdnc/순열의개수.java
@@ -0,0 +1,11 @@
+package hubdnc;
+
+public class 순열의개수 {
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
