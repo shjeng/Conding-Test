caa672e3ebbf6c14213eb9882d671907d3eff342
Jeong Sihwa <134994387+shjeng@users.noreply.github.com>
2025-04-03 22:19:50 +0900

    [Bronze V] Title: A/B, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bufferedReader.readLine().split(" ");
+
+        System.out.println(Double.parseDouble(s[0]) / Integer.parseInt(s[1]));
+    }
+
+
+}
+
